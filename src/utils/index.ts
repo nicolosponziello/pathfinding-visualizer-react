709e2de8f577433767672ad7e65d0a3dab2c2e90
nicolosponziello <nicolosponziello@gmail.com>
2020-11-10 15:45:52 +0100

    add basic utils

---
+++ src/utils/index.ts
@@ -0,0 +1,13 @@
+import { NodeState } from "../NodeState";
+
+export function randomInteger(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function randomEnum<T>(en: T): T[keyof T] {
+  let values = (Object.keys(en)
+    .map((n) => Number.parseInt(n))
+    .filter((n) => !Number.isNaN(n)) as unknown) as T[keyof T][];
+  let randomIndex = randomInteger(0, values.length);
+  return values[randomIndex];
+}

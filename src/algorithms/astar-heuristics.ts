e2c2bdfe52fbac9171726e6c80ddc1792f4f459f
nicolosponziello <nicolosponziello@gmail.com>
2020-11-21 11:01:29 +0100

    astar: implement some euristics for upcoming a* algo

---
+++ src/algorithms/astar-heuristics.ts
@@ -0,0 +1,34 @@
+import { Coordinate } from "../components/Node/node_data";
+
+const euclidean = (start: Coordinate, end: Coordinate): number => {
+  return Math.sqrt(Math.pow(start.r + end.r, 2) + Math.pow(start.c + end.c, 2));
+};
+
+const diagonal = (start: Coordinate, end: Coordinate): number => {
+  return Math.max(Math.abs(start.r - end.r), Math.abs(start.c - end.c));
+};
+
+const manhattan = (start: Coordinate, end: Coordinate): number => {
+  return Math.abs(start.r - end.r) + Math.abs(start.c - end.c);
+};
+
+export enum Euristic {
+  EUCLIDEAN,
+  MANHATTAN,
+  DIAGONAL,
+}
+
+export const calculateHeuristic = (
+  type: Euristic,
+  start: Coordinate,
+  end: Coordinate
+): number => {
+  switch (type) {
+    case Euristic.DIAGONAL:
+      return diagonal(start, end);
+    case Euristic.EUCLIDEAN:
+      return euclidean(start, end);
+    case Euristic.MANHATTAN:
+      return manhattan(start, end);
+  }
+};
